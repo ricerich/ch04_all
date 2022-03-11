362e9bdfd35e0c16cfb942debf5076686e58052d
Joseph Yoon <ricerich@gmail.com>
2022-03-11 15:22:11 +0900

    계산기 예제 1차 완료

--- ch04_calculation/src/main/java/com/example/ch04_calculation/MainActivity.kt
+++ ch04_calculation/src/main/java/com/example/ch04_calculation/MainActivity.kt
@@ -4,22 +4,90 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
-    lateinit  var edit1 : EditText;
-    lateinit  var edit2 : EditText;
+    //1.멤버변수
 
-    lateinit  var btnAdd : Button;
-    lateinit  var btnSub : Button;
-    lateinit  var btnMul : Button;
-    lateinit  var btnDiv : Button;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    //2.(멤버)메소드 - onCreate()
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
         title = "초간단 계산기"
+
+        //1.kotlin객체 만들기
+        //2.xml객체를 가져와서, kotlin 객체에 연결(바인딩)
+        //3.kotlin객체로 이벤트처리~!
+
+        //지역변수 자리
+
+        // 1. + 2. -> kotlin 객체를 만들면서, 동시에 xml객체를 연결(바인딩)
+        var edt1 : EditText = findViewById(R.id.Edit1)
+        var edt2 : EditText = findViewById(R.id.Edit2)
+
+        var btn1 : Button = findViewById(R.id.BtnAdd)
+        var btn2 : Button = findViewById(R.id.BtnSub)
+        var btn3 : Button = findViewById(R.id.BtnMul)
+        var btn4 : Button = findViewById(R.id.BtnDiv)
+
+        var tv1 : TextView = findViewById(R.id.TextResult)
+
+        //3.kotlin객체에 이벤트 처리
+
+        btn1.setOnTouchListener { view, motionEvent ->
+
+            var val1 = edt1.text;
+            var val2 = edt2.text;
+
+            var str1 = val1.toString()
+            var str2 = val2.toString()
+
+//            int n1 = Integer.parseInt(str1)
+            var n1 = str1.toInt()
+            var n2 = str2.toInt()
+
+            var n3 = n1 + n2;
+
+            tv1.setText("계산 결과 : "+n3)
+
+            false
+        }
+
+        btn2.setOnTouchListener { view, motionEvent ->
+            var n1 = edt1.text.toString().toInt()
+            var n2 = edt2.text.toString().toInt()
+            var n3 = n1 - n2
+
+            tv1.setText("계산 결과 : "+n3)
+
+            false
+        }
+
+        btn3.setOnTouchListener { view, motionEvent ->
+            var n1 = edt1.text.toString().toInt()
+            var n2 = edt2.text.toString().toInt()
+            var n3 = n1 * n2
+
+            tv1.setText("계산 결과 : "+n3)
+
+            false
+        }
+
+        btn4.setOnTouchListener { view, motionEvent ->
+            var n1 = edt1.text.toString().toInt()
+            var n2 = edt2.text.toString().toInt()
+            var n3 = n1 / n2
+
+            tv1.setText("계산 결과 : "+n3)
+
+            false
+        }
+
+
+
 
     }
 }
